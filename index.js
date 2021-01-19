83e659b6c0ec933e03af4ed86d4c1b5d23c2f866
velkog <60084050+velkog@users.noreply.github.com>
2021-01-19 03:34:13 -0800

    Update index.js

--- index.js
+++ index.js
@@ -56,29 +56,28 @@ client.on("message", message => {
 
     if ((message.sender === TIMBOSID || content.includes("timbos")) 
         && (content.includes("mod") || content.includes("admin"))) {
-          let branch = Math.floor(Math.random() * 5);
-          switch(branch) {
-            case 0:
-              message.channel.send("No.")
-              break;
-            case 1:
-              message.channel.send("Nah.")
-              break;
-            case 2:
-              message.channel.send("Nope.")
-              break;
-            case 3:
-              message.channel.send("Negativo.")
-              break;
-            case 4:
-              message.channel.send("Alright, fine.")
-              setTimeout(function(){
-                message.channel.send("LOL, jk, no.")
-              }, 1500);
-              break;
-          default:
-            break;
-        }
+      let branch = Math.floor(Math.random() * 5);
+      switch(branch) {
+        case 0:
+          message.channel.send("No.")
+          break;
+        case 1:
+          message.channel.send("Nah.")
+          break;
+        case 2:
+          message.channel.send("Nope.")
+          break;
+        case 3:
+          message.channel.send("Negativo.")
+          break;
+        case 4:
+          message.channel.send("Alright, fine.")
+          setTimeout(function(){
+            message.channel.send("LOL, jk, no.")
+          }, 1500);
+          break;
+        default:
+          break;
       }
     }
     
