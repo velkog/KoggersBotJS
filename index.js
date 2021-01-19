45cd0ce3fdde7811f492916afce991d08fefe835
velkog <60084050+velkog@users.noreply.github.com>
2021-01-19 03:32:14 -0800

    Update index.js

--- index.js
+++ index.js
@@ -54,10 +54,8 @@ client.on("message", message => {
         message.channel.send("Yeah, shut up Dank")
     }
 
-    if (message.sender === TIMBOSID 
-        || content.includes("timbos")) 
-        && (content.includes("mod") 
-        || content.includes("admin")) {
+    if ((message.sender === TIMBOSID || content.includes("timbos")) 
+        && (content.includes("mod") || content.includes("admin"))) {
           let branch = Math.floor(Math.random() * 5);
           switch(branch) {
             case 0:
