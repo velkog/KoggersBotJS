9157d0fda526cb23837178ef7e82e3f12c89ee2f
Velkog <60084050+velkog@users.noreply.github.com>
2021-08-22 05:27:21 -0700

    Update index.js

--- index.js
+++ index.js
@@ -28,14 +28,8 @@ client.on("ready", () => {
 });
 
 client.on("message", message => {
-    if(message.member.roles.cache.some(role => role.id === "878963262274818068")) {
+    if(message.member.roles.cache.some(role => role.name === "Shadow Banned")) {
         console.log(1);
-    }
-    if(message.member.roles.cache.some(role => role.id === 878963262274818068)) {
-        console.log(2);
-    }
-    if(message.member.roles.cache.some(role => role.id === "&878963262274818068")) {
-        console.log(3);
     }
     //console.log(SHADOW_BANNED_ROLE);
     if (message.channel.id === FART_CHANNEL && message.content !== FART_MSG) {
